8db0d6126d2723a4f2f07569b41f203ac95fd4b9
samarthsoni3002 <samarthsoni3002@gmail.com>
2023-07-08 11:40:54 +0530

    Interface completed

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 from sklearn.ensemble import GradientBoostingRegressor
-
+import sklearn
+print(sklearn.__version__)
 
 app = Flask(__name__)
 
@@ -18,26 +19,20 @@ def home():
     return render_template("index.html")
  
 
-@app.route("/predict",methods=["POST"])
-def predict():
-    data = request.json["data"]
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler_model.transform(np.array(list(data.values())).reshape(1,-1))
-    print(new_data)
-    output = insurance_model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
-            
+
 @app.route("/predict_model",methods=["POST"])
 def predict_model():
-    data = [float(x) for x in request.form.values()]
+    age = float(request.form["age"])
+    bmi = float(request.form["bmi"])
+    smoker = int(request.form.get("smoker",0))
+    
+    data = [age, bmi, smoker]
+    
     print(data)
-    final_input = scaler_model.transform((np.array(data).reshape(1,-1)))
-    print(final_input)
+    final_input = scaler_model.transform(np.array(data).reshape(1, -1))
     output = insurance_model.predict(final_input)
-    print(output)
-    return render_template("index.html",prediction_text = "The predicted price is {}".format(output[0]))
+
+    return render_template("index2.html", prediction_text="The predicted price is {}".format(output[0]))
 
 if __name__=="__main__":
     app.run(debug=True)
