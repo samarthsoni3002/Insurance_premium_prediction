44abb7cac057a893af9d0482533740c8c413bde7
samarthsoni3002 <samarthsoni3002@gmail.com>
2025-05-05 21:36:19 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import matplotlib as plt
 import seaborn as sns
 from sklearn.ensemble import GradientBoostingRegressor
 import sklearn
+import os
 print(sklearn.__version__)
 
 app = Flask(__name__)
@@ -32,5 +33,8 @@ def predict_model():
 
     return render_template("index2.html", prediction_text="The predicted price is {}".format(output[0]))
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
